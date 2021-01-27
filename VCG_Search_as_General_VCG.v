fe038c24bee4961df33d2763a6037386c9e0f412
jouvelot <pierre.jouvelot@mines-paristech.fr>
2021-01-27 16:21:11 +0100

    Modularize

--- VCG_Search_as_General_VCG.v
+++ VCG_Search_as_General_VCG.v
@@ -2,19 +2,12 @@
 
   VCG_Search_as_General_VCG.v
 
-  A formalization project for the Vickrey‑Clarke‑Groves auction mechanism, 
+  A formalization project for the Vickrey‑Clarke‑Groves auctions, 
   targeting:
 
-  - a specification of the "General VCG" algorithm;
+  - a specification of the "VCG for Search" auction variant.
 
-  - a proof of its key properties:
-    . agent rationality,
-    . no positive transfer,
-    . and truthfulness;
-
-  - a specification of the "VCG for Search" auction variant;
-
-  This embedding of "VCG for Search" into "General VCG" should ease proofs
+  This embedding of "VCG for Search" into the "General VCG" mechanism should ease proofs
   that this variant also enjoys these key properties. 
 
   See Tim Roughtgarden lecture notes for more details.
@@ -36,7 +29,9 @@
 
 From Coq Require Import Init.Prelude Unicode.Utf8.
 From mathcomp Require Import all_ssreflect.
-From mathcomp Require Import fingroup.perm.
+From mathcomp Require Import fingroup.perm. 
+
+Load General_VCG_mechanism.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -62,16 +57,6 @@ have f: (b1 - b2) * (c1 - c2) = b1 * c1 + b2 * c2 - (b1 * c2 + b2 * c1).
   rewrite !orbF => ltc2c1 ltb2b1.
   apply: ltnW.
   by rewrite -subn_gt0 -f muln_gt0 2!subn_gt0 ltb2b1 ltc2c1.
-Qed.
-
-Lemma max_monotonic 
-  (T : finType) 
-  (F F' : T -> nat) 
-  (ltFF' : forall o : T, F o <= F' o) :
-  \max_o F o <= \max_o F' o.
-Proof.
-elim/big_ind2: _ => // m1 m2 n1 n2  h1 h2. 
-by rewrite geq_max !leq_max h1 h2 orbT.
 Qed.
 
 Lemma big_split_subn k (P : 'I_k -> bool) F1 F2
@@ -119,174 +104,6 @@ by rewrite size_tuple ltn_ord.
 Qed.
 
 End Aux.
-
-Section Ordinal.
-
-Definition addS_ord m (j : 'I_(m.+1)) :=
-  match j with
-  | Ordinal v p => if v == m then m else v.+1
-  end.
-
-Lemma lt_j1_m m (j : 'I_(m.+1)) (ltjm : j < m) : j.+1 < m.+1.
-Proof. by []. Qed.
-
-Definition proper_addS_ord m (j : 'I_(m.+1)) (ltjm : j < m) :=
-  Ordinal (lt_j1_m ltjm).
-
-Lemma eq_proper_addS m (j : 'I_(m.+1)) (ltjm : j < m) :
-  @addS_ord m j = @proper_addS_ord m j ltjm.
-Proof.
-case: j ltjm => j ltjn /=.
-by move/ltn_eqF => ->.
-Qed.
-
-Lemma ltn_addS_ord m (j : 'I_m.+1) : addS_ord j < m.+1.
-Proof.
-move: j => [j ltjm1] //=.
-case eqjm: (j == m).
-by rewrite ltnSn.
-have //: j < m. 
-  by move: eqjm; rewrite ltn_neqAle => ->. 
-Qed.
-
-Definition ord_succ m (j : 'I_(m.+1)) := Ordinal (@ltn_addS_ord m j).
-  
-Lemma lt_ord_succ m (x : 'I_m.+1) : x < m -> x < ord_succ x.
-Proof.
-case: x => [s p] /= /ltn_eqF ->.
-by rewrite ltnS.
-Qed.
-
-Lemma le_ord_succ m (x : 'I_m.+1) : x <= ord_succ x.
-Proof.
-case: x => [s p] /=. 
-case: (s == m); last by rewrite leqnSn.
-by rewrite -ltnS.
-Qed.
-
-Lemma ltn_subS_ord m (j : 'I_m) : j.-1 < m.
-Proof. by move: (leq_pred j) (ltn_ord j); apply: leq_ltn_trans. Qed.
-
-Definition ord_pred m j := Ordinal (@ltn_subS_ord m j).
-
-Lemma below_last_ord m (j : 'I_(m.+1)) : (j != ord_max) = (j < m).
-Proof.
-by move: j => [j p]; rewrite -(inj_eq val_inj) ltn_neqAle -ltnS p andbT.
-Qed.
-
-Lemma cancel_ord_pred m (s : 'I_m.+1) : 0 < s -> ord_succ (ord_pred s) = s.
-Proof.
-move: s => [s p] /=.
-move=> lt0s.
-apply: ord_inj => //=.
-rewrite prednK //. 
-have/ltn_eqF -> //: s.-1 < m.
-  move: p.
-  by rewrite -[X in X < m.+1](@prednK s).
-Qed.
-
-Definition last_ord m := Ordinal (ltnSn m).
-
-Lemma cancel_ord_succ m (s : 'I_m.+1) :
-  s < last_ord m -> ord_pred (ord_succ s) = s.
-Proof.
-move: s => [s p] /=.
-move=> ltsm. 
-apply: ord_inj => //=.
-by move/ltn_eqF: ltsm => ->.
-Qed.
-
-Lemma big_cropr m F (i : nat) :
-  \sum_(s < m.+1 |(i <= s) && (s != ord_max)) F s =
-    \sum_(s < m.+1 | i <= s < m) F s.
-Proof. by apply: eq_bigl => s; rewrite below_last_ord. Qed.
-
-Lemma sum0_gt_last m (F : 'I_m.+1 -> nat) : \sum_(s < m.+1 | m < s) F s = 0.
-Proof.
-rewrite (@eq_bigl _ _ _ _ _ (fun s : 'I_m.+1 => m < s) xpred0) => [|s].
-by rewrite big_pred0_eq. 
-apply: negbTE.
-by rewrite -leqNgt (leq_ord s). 
-Qed.
-
-End Ordinal.
-
-Variable (n'' : nat).
-Definition n' := n''.+1.
-Definition n := n'.+1.
-
-Definition A := 'I_n.
-
-Section Agent.
-
-Definition last_agent : A := ord_max.
-
-Definition agent_succ j : A := @ord_succ n' j.
-
-Definition agent_pred j := @ord_pred n j.
-
-Lemma sum_predT m (F : 'I_(m.+1) -> nat) :
-  \sum_(j < m.+1) F j = \sum_(j < m.+1 | predT j) F j.
-Proof. by []. Qed.
-
-Lemma reindex_ge_i (i : A) m (F : 'I_m.+1 -> nat) :
-  0 < m ->
-    i < last_ord m ->
-      \sum_(s < m.+1 | i < s) F s =
-        \sum_(s < m.+1 | i <= s < m) F (ord_succ s).
-Proof.
-move=> lt0m ltil.
-rewrite (bigD1 ord_max) //=.
-rewrite [in RHS](bigD1 (ord_pred ord_max)) //=; last first.
-apply/andP. split; last by rewrite ltn_predL.
-rewrite -ltnS (ltn_predK lt0m); first by exact: ltil.
-rewrite cancel_ord_pred //=; last first.
-congr (_ + _).
-pose P := fun s : 'I_m.+1 => (i < s) && (s != ord_max).
-rewrite (@reindex_onto _ _ _ _ _
-    (@ord_succ m) (@ord_pred m.+1) P) //=; last first. 
-- move=> s /andP [ltis nesx].
-  rewrite cancel_ord_pred //.
-  exact: (leq_ltn_trans (leq0n i)).
-- apply: eq_bigl => s.
-  rewrite /P below_last_ord //=.
-  case: s => [s' p] /=.
-  rewrite -![X in _ = _ && ~~ X](inj_eq val_inj) /=.
-  - case eqs'm: (s' == m).
-    rewrite ltnn andbF andFb.
-    move: eqs'm => /eqP ->.
-    by rewrite ltnn andbF andFb.
-  - rewrite cancel_ord_succ //= ?eqxx ?andbT.
-    rewrite ltnS -ltn_predRL.
-    rewrite -andbA [X in _ = _ && X]andbC.
-    rewrite -(@prednK m lt0m) ltnS.
-    by rewrite -ltn_neqAle.
-  move: eqs'm p; rewrite ltnS leq_eqVlt => ->.
-  by rewrite orFb.
-Qed.
-
-Lemma agent_succ_mon (j j' : A) : j <= j' -> agent_succ j <= agent_succ j'.
-Proof.
-move=> lejj' /=.
-have [] := boolP (j' != last_agent) => j'notlast.
-- rewrite !eq_proper_addS //=.
-  rewrite below_last_ord in j'notlast => //.
-  exact: leq_ltn_trans lejj' j'notlast.
-  by rewrite below_last_ord in j'notlast.
-- move: j'notlast; rewrite negbK => /eqP -> /=.
-  rewrite ifT //=.
-  exact: ltn_addS_ord.
-Qed.
-
-Lemma leq_eqVlt_agent (j j' : A) : (j <= j') = (j == j') || (j < j').
-Proof. exact: leq_eqVlt. Qed.
-
-Lemma lt_le_agent (j j' : A)
-  (jnotlast : j != last_agent) :
-  (j < j') = (agent_succ j <= j').
-Proof. by rewrite /= !eq_proper_addS; rewrite below_last_ord in jnotlast. Qed.
-
-End Agent.
 
 Ltac nth_split_i j ltji :=
   rewrite nth_cat size_takel ?size_tuple ?(ltnW ltn_ord) //;
@@ -508,129 +325,7 @@ Hypothesis bubble_sort_spec : forall m m' (t : m.+1.-tuple 'I_m'.+1),
     let xo := bubble_sort t i1i2s in
     xo.1 /\ up_sorted_tuple xo.2. 
 
-Module VCG.
-
-(* General VCG and key properties. *)
-
-Section Algorithm.
-
-Variable (O : finType) (o0 : O) (i : A).
-
-Definition bidding := {ffun O -> nat}.
-Definition biddings := n.-tuple bidding.
-
-Variable (bs : biddings).
-Local Notation "'bidding_ j" := (tnth bs j) (at level 10).
-
-Implicit Types (o : O) (bs : biddings).
-
-Definition bidSum o := \sum_(j < n) 'bidding_j o.
-Definition bidSum_i o := \sum_(j < n | j != i) 'bidding_j o.
-
-Definition oStar := [arg max_(o > o0) (bidSum o)].
-
-Definition welfare_with_i := bidSum_i oStar.
-
-Definition welfare_without_i := \max_o bidSum_i o.
-
-Definition price := welfare_without_i - welfare_with_i.
-
-(* Alternative definition for welfare_without_i (i=0). *)
-
-Definition bs'' : biddings :=
-  [tuple [ffun o : O => (if j != i then 'bidding_j o else 0)]| j < n].
-
-Notation "'bidding''_ j" := (tnth bs'' j) (at level 10).
-
-Definition bidSum'' o := \sum_(j < n) 'bidding''_j o.
-
-Definition welfare_without_i'' := \max_o bidSum'' o.
-
-Definition price'' := welfare_without_i'' - welfare_with_i.
-
-Lemma eq_bidSum'' o : bidSum'' o = bidSum_i o.
-Proof. 
-rewrite /bidSum'' /bidSum_i.
-rewrite (big_mkcond (fun j => j != i)) //=.
-set F2 := (X in _ = \sum_(i0 < n) X i0).
-rewrite (eq_bigr F2) => [//=|j _].
-by rewrite tnth_mktuple ffunE /F2.
-Qed.
-
-Lemma eq_welfare'' : welfare_without_i = welfare_without_i''.
-Proof.
-rewrite /welfare_without_i /welfare_without_i''.
-rewrite (eq_bigr bidSum'') => [//=|o _].
-by rewrite eq_bidSum''.
-Qed.
-
-Lemma eq_price'' : price = price''.
-Proof. by rewrite /price'' -eq_welfare''. Qed.
-
-(* Useful properties. *)
-
-Lemma bidSumD1 o : bidSum o = 'bidding_i o + bidSum_i o.
-Proof. by rewrite /bidSum (bigD1 i). Qed.
-
-Theorem no_positive_transfer : (* 0 <= price *)
-  welfare_with_i <= welfare_without_i.
-Proof. exact: leq_bigmax. Qed.
-
-Variable (value : bidding).
-
-Hypothesis value_is_bid : 'bidding_i = value.
-
-Lemma rational : price <= value oStar.
-Proof.
-rewrite -value_is_bid leq_subLR addnC -bidSumD1 -bigmax_eq_arg //.
-have rebate_ge0 o : bidSum_i o <= bidSum o.
-  by rewrite (bidSumD1 o) leq_addl.
-exact: max_monotonic.
-Qed.
-
-End Algorithm.
-
-Section Truthfulness.
-
-Variable (O : finType) (o0 : O) (i : A).
-
-Variable (bs : biddings O).
-
-Notation "'bidding_ j" := (tnth bs j) (at level 10).
-Notation "'o* bs" := (@oStar O o0 bs) (at level 10).
-
-Variable (value : bidding O).
-
-Definition utility bs := value ('o* bs) - (@price O o0 i bs).
-
-Definition differ_only_i bs' := forall j, j != i -> tnth bs' j = 'bidding_j.
-
-Theorem truthful bs' :
-  'bidding_i =1 value ->
-    differ_only_i bs' ->
-      utility bs' <= utility bs.
-Proof.
-move=> value_is_bid diff.
-rewrite /utility /price.
-have eq_Sum_i : bidSum_i i bs  =1 bidSum_i i bs'.
-  move=> o.
-  apply: eq_bigr => j jnoti.
-  rewrite /differ_only_i in diff.
-  by move: diff => ->.
-rewrite /price /welfare_without_i /welfare_with_i.
-rewrite (@eq_bigr _ _ _ _ _ _ (bidSum_i i bs) (bidSum_i i bs')) //.
-rewrite ?2!subnBA ?leq_sub2r //.
-rewrite -2!value_is_bid -eq_Sum_i //.
-rewrite -2!bidSumD1 /('o* bs) -bigmax_eq_arg => //.
-by apply: bigmax_sup.
-rewrite eq_Sum_i.
-exact: bigmax_sup.
-exact: bigmax_sup.
-Qed.
-
-End Truthfulness.
-
-End VCG.
+(* VCG for Search algorithm *)
 
 Variable (k' : nat).
 Definition k := k'.+1.
